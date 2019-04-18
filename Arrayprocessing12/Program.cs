0496466b9e1ab47627123874ba5541ac9fbbd99c
visualstudio <visualstudio@230-PC29>
2019-04-18 15:27:11 -0400

    Array

--- Arrayprocessing12/Program.cs
+++ Arrayprocessing12/Program.cs
@@ -10,6 +10,25 @@ namespace Arrayprocessing12
     {
         static void Main(string[] args)
         {
+
+            int[] arr = { 17, 11, 9, 3, 4, 11, 6, 11, 8, 7 };
+            int arr_size = arr.Length;
+
+            printRepeating(arr, arr_size);
+        }
+        static void printRepeating(int[] arr, int size)
+        {
+            int i, j;
+
+            Console.WriteLine("Repeated Elements are :");
+            for (i = 0; i < size; i++)
+            {
+                for (j = i + 1; j < size; j++)
+                {
+                    if (arr[i] == arr[j])
+                        Console.WriteLine(arr[i] + " ");
+                }
+            }
         }
     }
 }
